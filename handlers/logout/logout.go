db84385ed2fc497bbb2dbf38f9b5043c5a06d5c2
Siddhant Vij <siddhantvij201@gmail.com>
2024-08-14 01:15:07 +0530

    auth0 logout flow implemented

--- handlers/logout/logout.go
+++ handlers/logout/logout.go
@@ -1,1 +1,54 @@
 package logout
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/siddhant-vij/Auth0-WebApp-Demo/config"
+	"github.com/siddhant-vij/Auth0-WebApp-Demo/utils"
+)
+
+func HandleLogout(w http.ResponseWriter, r *http.Request, cfg *config.Config) {
+	logoutUrl, err := url.Parse("https://" + cfg.Domain + "/v2/logout")
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	scheme := "http"
+	if r.TLS != nil {
+		scheme = "https"
+	}
+
+	returnTo, err := url.Parse(scheme + "://" + r.Host)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	params := url.Values{}
+	params.Add("returnTo", returnTo.String())
+	params.Add("client_id", cfg.ClientID)
+	logoutUrl.RawQuery = params.Encode()
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "access_token",
+		Value:    "",
+		MaxAge:   0,
+		Secure:   false,
+		HttpOnly: true,
+	})
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     "state",
+		Value:    "",
+		MaxAge:   0,
+		Secure:   false,
+		HttpOnly: true,
+	})
+
+	cfg.UserProfile.Nickname = ""
+	cfg.UserProfile.Picture = ""
+
+	http.Redirect(w, r, logoutUrl.String(), http.StatusSeeOther)
+}
--- router/router.go
+++ router/router.go
@@ -8,6 +8,7 @@ import (
 	"github.com/siddhant-vij/Auth0-WebApp-Demo/handlers/callback"
 	"github.com/siddhant-vij/Auth0-WebApp-Demo/handlers/home"
 	"github.com/siddhant-vij/Auth0-WebApp-Demo/handlers/login"
+	"github.com/siddhant-vij/Auth0-WebApp-Demo/handlers/logout"
 	"github.com/siddhant-vij/Auth0-WebApp-Demo/handlers/user"
 	"github.com/siddhant-vij/Auth0-WebApp-Demo/middlewares"
 	"github.com/siddhant-vij/Auth0-WebApp-Demo/utils"
@@ -43,6 +44,10 @@ func RegisterRoutes(mux *http.ServeMux) {
 
 	mux.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		callback.ServeCallbackPage(w, r, auth, cfg)
+	})
+
+	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		logout.HandleLogout(w, r, cfg)
 	})
 
 	mux.Handle("/user", middlewares.IsAuthenticated(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
